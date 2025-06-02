dd418d65e0af0bc538556993003a735371ae15ef
Tommy <roughcookie@gmail.com>
2025-06-02 17:07:56 +0000

    hanging fix

--- main.go
+++ main.go
@@ -9,18 +9,22 @@ import (
 var audioStream = make(chan []byte)
 
 func sseHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Starting new SSE connection")
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(http.StatusOK)
 
 	flusher, ok := w.(http.Flusher)
+	flusher.Flush()
 	if !ok {
 		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
 		return
 	}
 
 	fmt.Fprintf(w, "retry: 1000\n\n") // Tell client to retry every second if connection is lost
+	flusher.Flush()
 
 	clientChan := make(chan []byte)
 	go func() {
@@ -49,7 +53,9 @@ func sseHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func SendBytes(bytes []byte) {
+	log.Printf("SSE_SEND: Attempting to send %d bytes to audioStream", len(bytes))
 	audioStream <- bytes
+	log.Println("SSE_SEND: Successfully sent to audioStream")
 }
 
 func Start() {
