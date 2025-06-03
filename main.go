afb3c4ec16fd11de6ae535a4508f03348b561722
Tommy <roughcookie@gmail.com>
2025-06-03 17:49:47 +0000

    Maybe use one goroutine?

--- main.go
+++ main.go
@@ -27,41 +27,24 @@ func sseHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "retry: 1000\n\n") // Tell client to retry every second if connection is lost
 	flusher.Flush()
 
-	clientChan := make(chan []byte)
-	go func() {
-	    	log.Println("SSE_GOROUTINE: Started. Ranging over audioStream.")
-    		for data := range audioStream {
-       			log.Printf("SSE_GOROUTINE: Got %d bytes from audioStream. Attempting to send to clientChan.", len(data))
-        		select {
-        			case clientChan <- data:
-            				log.Printf("SSE_GOROUTINE: Successfully sent %d bytes to clientChan.", len(data))
-        			case <-r.Context().Done(): // If the client request is cancelled
-            				log.Println("SSE_GOROUTINE: Client context done. Stopping.")
-           	 			return
-        		}
-    		}
-		log.Println("SSE_GOROUTINE: audioStream was closed or loop ended.")
-	}()
-
-	defer func() {
-        	close(clientChan)
-		log.Println("Client disconnected")
-	}()
-
 	for {
-		select {
-		case <-r.Context().Done():
-			log.Println("Client disconnected via context")
-			return
-		case audioData := <-clientChan:
-			if len(audioData) > 0 {
-				fmt.Fprintf(w, "data: %s\n\n", base64.StdEncoding.EncodeToString(audioData))
-				flusher.Flush()
-				log.Println("SSE_HANDLER_LOOP: Successfully flushed data to client/Nginx.")
-			}
-    			flusher.Flush()
-		}
+    		select {
+    			case <-r.Context().Done():
+        			log.Println("Client disconnected via context")
+        			return
+    			case data, ok := <-audioStream:
+        			if !ok {
+            				log.Println("audioStream closed")
+            				return
+        			}
+        			if len(data) > 0 {
+            				fmt.Fprintf(w, "data: %s\n\n", base64.StdEncoding.EncodeToString(data))
+            				flusher.Flush()
+            				log.Println("SSE_HANDLER_LOOP: Successfully flushed data to client.")
+        			}
+    		}
 	}
+
 }
 
 func SendBytes(bytes []byte) {
